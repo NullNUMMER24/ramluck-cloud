92d6e7039d066f47d117d3df88217f1085e10306
NullNUMMER24 <j.rohrbach@sensemail.ch>
2025-08-16 09:27:25 +0200

    started developing ramluck in Tanzania xD

---
+++ backend/GoCode/provisioning/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nix flake for building NixOS Proxmox images";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        # Dev shell with the needed tools
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixos-generators
+            pkgs.gawk
+            pkgs.git
+            pkgs.curl
+          ];
+
+          shellHook = ''
+            echo "🧪 Welcome to the NixOS image build shell!"
+          '';
+        };
+
+        # Optional: expose nixos-generators as a top-level package too
+        packages.default = pkgs.nixos-generators;
+      });
+}
+
